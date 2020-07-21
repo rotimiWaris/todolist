3e8204f1ccde936016eeed64df21b9629ea97f6d
Rotimi Waris <warisrotimi@gmail.com>
2020-07-21 12:52:56 +0100

    Settings

--- twttodolist/settings.py
+++ twttodolist/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p&-@e@a!m0-wo7^qmv5toqfol^q8+8$)9ce$uyn^-$nn(vgw(3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", False))
+DEBUG = bool(os.environ.get("DEBUG", ""))
 
 ALLOWED_HOSTS = []
 
