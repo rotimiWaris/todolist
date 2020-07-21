3a44180d995a8164d079187ff1f16689b6f77767
Rotimi Waris <warisrotimi@gmail.com>
2020-07-21 18:56:53 +0100

    Settings

--- twttodolist/settings.py
+++ twttodolist/settings.py
@@ -151,8 +151,8 @@ django_heroku.settings(locals())
 if ENVIRONMENT == 'production':
     # DEBUG = bool(os.environ.get('DEBUG', False))
     DEBUG = bool(os.environ.get('DEBUG', ''))
-    DEBUG = os.environ.get('DEBUG', False)
-    DEBUG = os.getenv('DEBUG', False) == 'True'
+    # DEBUG = os.environ.get('DEBUG', False)
+    # DEBUG = os.getenv('DEBUG', False) == 'True'
     # SECRET_KEY = os.getenv('SECRET_KEY')
     SESSION_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
