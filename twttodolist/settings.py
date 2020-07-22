87eab768ab39e71090aba26be067c35a1b4cfc8f
Rotimi Waris <warisrotimi@gmail.com>
2020-07-22 16:19:52 +0100

    Settings

--- twttodolist/settings.py
+++ twttodolist/settings.py
@@ -167,3 +167,4 @@ if ENVIRONMENT == 'production':
     SECURE_REDIRECT_EXEMPT = []
     SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
